54d9300b0670e9769cfc155a00c6f8ae29cdb1cd
Sushant Belsare <30728571+sushantbelsare@users.noreply.github.com>
2024-10-07 21:14:28 -0400

    Time: 5 ms (38.64%) | Memory: 3.3 MB (56.82%) - LeetSync

---
+++ 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.go
@@ -0,0 +1,36 @@
+/*
+*   input = "AABB" output = 0
+*   input = "AAACDBB" output = 1
+*   input = "CDCDAB" output = 0
+*   input = "CADB" output = 4
+*
+*   approach:
+*       if s[i] + s[i + 1] in substring
+*           remove (i, i + 1)
+*
+*       else
+*           i = i + 1 
+*/
+
+import "fmt"
+
+func minLength(s string) int {
+    n := len(s)
+    i := 1
+    j := 0
+    stack := string(s[0])
+
+    for i < n {
+        if len(stack) > 0 && ((stack[j] == 'A' && s[i] == 'B') || (stack[j] == 'C' && s[i] == 'D')) {
+            stack = stack[:j]
+            j = j - 1
+        }else{
+            stack = stack + string(s[i])
+            j = j + 1
+        }
+
+        i = i + 1
+    }
+
+    return j + 1
+}
