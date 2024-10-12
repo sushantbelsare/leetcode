83df4eea4d5d8ba8c32a83d5158c1ef8f99323bf
Sushant Belsare <30728571+sushantbelsare@users.noreply.github.com>
2024-10-12 18:18:48 -0400

    Time: 33 ms (91.49%) | Memory: 16.5 MB (89.77%) - LeetSync

---
+++ 179-largest-number/largest-number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        num_strings = [str(num) for num in nums]
+
+        num_strings.sort(key=lambda a: a * 10, reverse=True)
+
+        if num_strings[0] == "0":
+            return "0"
+
+        return "".join(num_strings)
