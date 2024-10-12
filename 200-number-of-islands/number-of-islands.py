58cb3e2ed8b65e2dedf3bea76337aa2f10becdad
Sushant Belsare <30728571+sushantbelsare@users.noreply.github.com>
2024-10-12 17:40:34 -0400

    Time: 253 ms (47.42%) | Memory: 18.9 MB (65.90%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -3,19 +3,18 @@ from collections import OrderedDict
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         m, n = len(grid), len(grid[0])
-        visited = set()
 
-        def dfs(i, j, visited):
+        def dfs(i, j):
             for dx, dy in zip([0, 1, 0, -1], [1, 0, -1, 0]):
-                if 0 <= i + dx < m and 0 <= j + dy < n and (i + dx, j + dy) not in visited and grid[i + dx][j + dy] == "1":
-                    visited.add((i + dx, j + dy))
-                    dfs(i + dx, j + dy, visited)
+                if 0 <= i + dx < m and 0 <= j + dy < n and grid[i + dx][j + dy] == "1":
+                    grid[i + dx][j + dy] = "0"
+                    dfs(i + dx, j + dy)
 
         num_islands = 0
         for i in range(m):
             for j in range(n):
-                if grid[i][j] == "1" and (i, j) not in visited:
-                    dfs(i, j, visited)
+                if grid[i][j] == "1":
+                    dfs(i, j)
                     num_islands += 1
 
         return num_islands
