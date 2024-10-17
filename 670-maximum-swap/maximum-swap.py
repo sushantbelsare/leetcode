713fea2f9d882a079454bfaef3c531945638001e
Sushant Belsare <30728571+sushantbelsare@users.noreply.github.com>
2024-10-17 09:40:19 -0400

    Time: 0 ms (100.00%) | Memory: 16.6 MB (10.80%) - LeetSync

--- 670-maximum-swap/maximum-swap.py
+++ 670-maximum-swap/maximum-swap.py
@@ -6,22 +6,17 @@ class Solution:
     #   - Input: 1112   Output: 2111
     #   - Input: 432123 Output: 433122  
     def maximumSwap(self, num: int) -> int:
-        num_str = list(str(num))
-        reversed_num_str = sorted(num_str, reverse=True)
-        i = 0
-        n = len(num_str)
-
-        while i < n and num_str[i] == reversed_num_str[i]:
-            i += 1
-
-        if i < n:
-            j = i + 1
-            swap_index = 0
-            while j < n:
-                if num_str[j] == reversed_num_str[i]:
-                    swap_index = j
-                j += 1
-            num_str[swap_index] = num_str[i]
-            num_str[i] = reversed_num_str[i]
-            return int(''.join(num_str))
+        num_str = str(num)
+        length = len(num_str)
+        for i, (m, n) in enumerate(zip(num_str, sorted(num_str, reverse=True))):
+            if m != n:
+                j = i + 1
+                swap_index = 0
+                while j < length:
+                    if num_str[j] == n:
+                        swap_index = j
+                    j += 1
+                num_str = num_str[:swap_index] + m + num_str[swap_index + 1:]
+                num_str = num_str[:i] + n + num_str[i + 1:]
+                return int(''.join(num_str))
         return num
